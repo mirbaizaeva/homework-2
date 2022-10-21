67b83d793713a93b622c658d4ac94b34254fa32d
Nurjamal <n.mirbaizaeva@gmail.com>
2022-10-21 21:01:18 +0600

    ff

--- homework 2/main.swift
+++ homework 2/main.swift
@@ -1,11 +1,35 @@
-//
-//  main.swift
-//  homework 2
-//
-//  Created by Nurjamal Mirbaizaeva on 20/10/22.
-//
+//Нахождение квадрата числа
+func findSquareNumber(number: Int){
+    let squareNumber = number * number
+    print("Квадрат числа \(number) = равен \(squareNumber)")
+}
+findSquareNumber(number: 40)
 
-import Foundation
+//Нахождение куба числа
+let number = "Введите число"
+func findCubeNumber(number: Int) {
+    let cubeNumber = number * number * number
+    print("Куб числа \(number) = равен \(cubeNumber)")
+}
+findCubeNumber(number: 25)
 
-print("Hello, World!")
+//Нахождение периметра куба
+func findCubePerimeternum(number: Int){
+    let cubePerimeternum = (number) * 8
+    print("Периметр куба равен \(cubePerimeternum)")
+}
+findCubePerimeternum(number: 15)
 
+//Нахождение площади прямоугольника
+func findarea(number: Int, num: Int){
+    let areaRectangle = (number + num) * 2
+    print("Площад прямоугольника равен \(areaRectangle)")
+}
+findarea(number: 13, num: 33)
+
+//Нахождение площади круга
+func findCircleSquare(radius: Double) {
+    let square = 2 * 3.14 * (radius * radius)
+    print("Площадь круга равен \(square)")
+}
+findCircleSquare(radius: 50)
